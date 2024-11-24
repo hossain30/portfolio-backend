19d8123398f0541c2455a684756b9a67ee874246
heyRubel <samsujjohahossain@gmail.com>
2024-11-24 14:08:54 +0530

    this is my portfolio

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+app.use(cors({
+  
+}));
+app.use(express.json());
+require("dotenv").config();
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.FROM_GMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+
+app.post("/contactUs", async (req, res) => {
+  const { name, phone, email, message } = req.body;
+
+  const mailOptions = {
+    from: process.env.FROM_GMAIL,
+    to: process.env.TO_GMAIL,
+    subject: `New message from ${name}`,
+    text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ status: 200 });
+  } catch (error) {
+    res.status(500).json({ status: 500 });
+  }
+});
+
+app.listen(PORT, () => {
+  // console.log(`Server is running on http://localhost:${PORT}`);
+});
